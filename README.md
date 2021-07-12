64b4dbdee2d04d91e9984225781420277bf29a7d
weedust <ldc4@vip.qq.com>
2021-07-12 21:27:30 +0800

    暂存

--- README.md
+++ README.md
@@ -7,7 +7,6 @@
 
 https://www.tailwindcss.cn/docs/guides/vue-3-vite
 
-
 npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
 
 npx tailwindcss init -p
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
-
+  <div class="w-screen">
+    <div class="mx-auto my-8" :style="{ width, height }">
+      <div class="h-1/4 bg-green-500"></div>
+      <div class="h-3/4 bg-gray-100"></div>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+import { reactive, ref, computed } from 'vue'
 
+const paperSizeMap = {
+  'A4': {
+    height: `${Math.SQRT2 * 80}vw`,
+    width: '80vw',
+  }
+}
+
+export default {
+  setup() {
+
+    const type = ref('A4');
+
+    const width = computed(() => {
+      return paperSizeMap[type.value].width;
+    });
+    const height = computed(() => {
+      return paperSizeMap[type.value].height;
+    })
+
+    return reactive({
+      width,
+      height
+    })
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
